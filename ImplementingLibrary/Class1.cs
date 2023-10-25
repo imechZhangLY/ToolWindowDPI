9f71712205c7dbf6ca24411760dde909db26c934
Luyao Zhang <zhangluyao@microsoft.com>
2023-10-25 12:47:26 +0800

    add type load exception demo

---
+++ ImplementingLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementingLibrary
+{
+    public class ImplementingClass : InterfaceLibrary.IClass1
+    {
+        public string GetText()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var implementingClass = new ImplementingLibrary.ImplementingClass();
+            Console.WriteLine(implementingClass.GetText());
+            Console.ReadLine();
+        }
+    }
+}
